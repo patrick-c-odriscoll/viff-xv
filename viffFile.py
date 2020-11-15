99bb634919c9fb25bedbd1a06fcd0e3852e12d2b
patrick-c-odriscoll <39496990+patrick-c-odriscoll@users.noreply.github.com>
2020-11-15 15:08:51 -0600

    Added comments to make it easier to read

--- viffFile.py
+++ viffFile.py
@@ -2,53 +2,70 @@ import sys, argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
-'''
-  Objectives:
+class viff():
+  """ Viff File reader and writer
     A viff or xv file is from Khoros/VisiQuest packages which has a header of
     1024 bytes and is BSQ formated raw raster file. We can parse the header
     information based upon the C struct definition provided in the source 
-    section. All of these elements are accessable in the class structure.
-   
-  Source: Quote from http://www.fileformat.info/format/viff/egff.htm
-    typedef struct _ViffHeader
-    { CHAR  FileId;            /* Khoros file ID value (always ABh)*/
-      CHAR  FileType;          /* VIFF file ID value (always 01h) */
-      CHAR  Release;           /* Release number (1) */
-      CHAR  Version;           /* Version number (0) */
-      CHAR  MachineDep;        /* Machine dependencies indicator */
-      CHAR  Padding[3];        /* Structure alignment padding (always 00h)*/
-      CHAR  Comment[512];      /* Image comment text */
-      DWORD NumberOfRows;      /* Length of image rows in pixels */
-      DWORD NumberOfColumns;   /* Length of image columns in pixels */
-      DWORD LengthOfSubrow;    /* Size of any sub-rows in the image */
-      LONG  StartX;            /* Left-most display starting position */
-      LONG  StartY;            /* Upper-most display starting position */
-      FLOAT XPixelSize;        /* Width of pixels in meters */
-      FLOAT YPixelSize;        /* Height of pixels in meters */
-      DWORD LocationType;      /* Type of pixel addressing used */
-      DWORD LocationDim;       /* Number of location dimensions */
-      DWORD NumberOfImages;    /* Number of images in the file */
-      DWORD NumberOfBands;     /* Number of bands (color channels) */
-      DWORD DataStorageType;   /* Pixel data type */
-      DWORD DataEncodingScheme;/* Type of data compression used */
-      DWORD MapScheme;         /* How map is to be interpreted */
-      DWORD MapStorageType;    /* Map element data type */
-      DWORD MapRowSize;        /* Length of map rows in pixels */
-      DWORD MapColumnSize;     /* Length of map columns in pixels */
-      DWORD MapSubrowSize;     /* Size of any subrows in the map */
-      DWORD MapEnable;         /* Map is optional or required */
-      DWORD MapsPerCycle;      /* Number of different   maps present */
-      DWORD ColorSpaceModel;   /* Color model used to represent image */
-      DWORD ISpare1;           /* User-defined field */
-      DWORD ISpare2;           /* User-defined field */
-      FLOAT FSpare1;           /* User-defined field */
-      FLOAT FSpare2;           /* User-defined field */
-      CHAR  Reserve[404];      /* Padding */
-      } VIFFHEADER;
-'''
+    section. All of these variables are accessable in the class structure.
 
-class viffFile():
-  def __init__(self, filename=None, data=None):
+    Inputs:
+    filename: [manditory]: filename to read from or write to
+    data:     [optional] : data to save formated as np.array with dimensions
+                [NumberOfImages,NumberOfBands,NumberOfColumns,NumberOfRows]
+
+    Functions:
+      read:  [Automatically run when class is initialized, when no data is given]
+        Returns a class with an element for each of the structure variables below. 
+        In addition ot the following:
+          dt   : np.dtype detailing the data format of the data.
+          data : the image with dimensions 
+                [NumberOfImages,NumberOfBands,NumberOfColumns,NumberOfRows]
+      write: [Automatically run when class is initialized, when data is given]
+        Writes the data in a viff formated file. This assumes that the data
+        is in the appropriate dimensions of:
+        [NumberOfImages,NumberOfBands,NumberOfColumns,NumberOfRows]
+    
+    Sources and Stucture: 
+    Quote from http://www.fileformat.info/format/viff/egff.htm
+      typedef struct _ViffHeader
+      { CHAR  FileId;            /* Khoros file ID value (always ABh)*/
+        CHAR  FileType;          /* VIFF file ID value (always 01h) */
+        CHAR  Release;           /* Release number (1) */
+        CHAR  Version;           /* Version number (0) */
+        CHAR  MachineDep;        /* Machine dependencies indicator */
+        CHAR  Padding[3];        /* Structure alignment padding (always 00h)*/
+        CHAR  Comment[512];      /* Image comment text */
+        DWORD NumberOfRows;      /* Length of image rows in pixels */
+        DWORD NumberOfColumns;   /* Length of image columns in pixels */
+        DWORD LengthOfSubrow;    /* Size of any sub-rows in the image */
+        LONG  StartX;            /* Left-most display starting position */
+        LONG  StartY;            /* Upper-most display starting position */
+        FLOAT XPixelSize;        /* Width of pixels in meters */
+        FLOAT YPixelSize;        /* Height of pixels in meters */
+        DWORD LocationType;      /* Type of pixel addressing used */
+        DWORD LocationDim;       /* Number of location dimensions */
+        DWORD NumberOfImages;    /* Number of images in the file */
+        DWORD NumberOfBands;     /* Number of bands (color channels) */
+        DWORD DataStorageType;   /* Pixel data type */
+        DWORD DataEncodingScheme;/* Type of data compression used */
+        DWORD MapScheme;         /* How map is to be interpreted */
+        DWORD MapStorageType;    /* Map element data type */
+        DWORD MapRowSize;        /* Length of map rows in pixels */
+        DWORD MapColumnSize;     /* Length of map columns in pixels */
+        DWORD MapSubrowSize;     /* Size of any subrows in the map */
+        DWORD MapEnable;         /* Map is optional or required */
+        DWORD MapsPerCycle;      /* Number of different   maps present */
+        DWORD ColorSpaceModel;   /* Color model used to represent image */
+        DWORD ISpare1;           /* User-defined field */
+        DWORD ISpare2;           /* User-defined field */
+        FLOAT FSpare1;           /* User-defined field */
+        FLOAT FSpare2;           /* User-defined field */
+        CHAR  Reserve[404];      /* Padding */
+        } VIFFHEADER;
+  """
+  def __init__(self, filename, data=None):
+    ''' initialization of the class '''
     self.filename = filename
     if data is not None:
       self.data = data
@@ -58,6 +75,7 @@ class viffFile():
     return
   
   def read(self):
+    ''' read the self.filename and populate the class structure '''
     f = open(self.filename,'rb')
     self.FileId   = f.read(1)
     self.FileType = f.read(1)
@@ -65,18 +83,18 @@ class viffFile():
     self.Version  = f.read(1)
     assert (self.FileId  == b'\xab') and (self.FileType == b'\x01') and \
            (self.Release == b'\x01'), \
-           'viffFile: '+self.filename+' is not a viff or xv file format'
+           'viff: '+self.filename+' is not a viff or xv file format'
     self.MachineDep = f.read(1)
     if   self.MachineDep == b'\x08':
-      self.endianness = 'little'
+      endianness = 'little'
     elif self.MachineDep == b'\x02':
-      self.endianness = 'big'
+      endianness = 'big'
     else:
       assert True, \
-             'viffFile: '+self.filename+' is not a viff or xv file format'
+             'viff: '+self.filename+' unsupported endianness'
     self.Padding = f.read(3)
     assert (self.Padding == b'\x00\x00\x00'), \
-           'viffFile: '+self.filename+' is not a viff or xv file format'
+           'viff: '+self.filename+' is not a viff or xv file format'
     self.Comment = f.read(512).decode('ASCII','ignore')
     self.NumberOfRows = int.from_bytes(f.read(4),byteorder='little')
     self.NumberOfColumns = int.from_bytes(f.read(4),byteorder='little')
@@ -84,7 +102,7 @@ class viffFile():
     self.StartX = f.read(4)
     self.StartY = f.read(4)
     assert (self.StartX == b'\xff\xff\xff\xff') and (self.StartY == b'\xff\xff\xff\xff'),\
-           'viffFile: Unkown values in StartX | StartY'
+           'viff: Unkown values in StartX | StartY'
     self.XPixelSize = f.read(4)
     self.YPixelSize = f.read(4)
     self.LocationType = f.read(4)
@@ -93,7 +111,7 @@ class viffFile():
     self.NumberOfBands = int.from_bytes(f.read(4),byteorder='little')
     self.DataStorageType = int.from_bytes(f.read(4),byteorder='little')
     if   self.DataStorageType == 0: # bit
-      assert True, 'viffFile: '+self.filename+' unsuported bit format'
+      assert True, 'viff: '+self.filename+' unsuported bit format'
     elif self.DataStorageType == 1: # uint8
       self.dt = np.dtype(np.uint8)
     elif self.DataStorageType == 2: # uint16
@@ -109,12 +127,14 @@ class viffFile():
     elif self.DataStorageType == 10:# double complex
       self.dt = np.dtype(np.cdouble)
     else:
-      assert True, 'viffFile: '+self.filename+' unkown type format: '+self.DataStorageType
-    if self.endianness == 'little':
+      assert True, 'viff: '+self.filename+' unkown type format: '+self.DataStorageType
+    if endianness == 'little':
       self.dt.newbyteorder('L')
-    elif self.endianness == 'big':
+    elif endianness == 'big':
       self.dt.newbyteorder('B')
     self.DataEncodingScheme = f.read(4)
+    if self.DataEncodingScheme != b'\x00\x00\x00\x00':
+      assert True, 'viff: '+self.filename+' unsupported DataEncodingScheme'
     self.MapScheme = f.read(4)
     self.MapStorageType = f.read(4)
     self.MapRowSize = f.read(4)
@@ -135,6 +155,7 @@ class viffFile():
     return
   
   def write(self,filename):
+    ''' write the data to the target file '''
     f = open(filename,'wb')
     f.write(b'\xab')                       # FileId
     f.write(b'\x01')                       # FileType
@@ -145,7 +166,7 @@ class viffFile():
     elif sys.byteorder == 'big':
       f.write(b'\x02')
     else:
-      print('viffFile: unkown endianness')
+      print('viff: unkown endianness')
     f.write(b'\x00\x00\x00')               # Padding
     f.write(b'\x00'*512)                   # Comment
     f.write(np.uint32(self.data.shape[3])) # NumberOfRows
@@ -159,7 +180,7 @@ class viffFile():
     f.write(b'\x00\x00\x00\x00')           # LocationDim
     f.write(np.uint32(self.data.shape[0])) # NumberOfImages
     f.write(np.uint32(self.data.shape[1])) # NumberOfBands
-    # TODO finish DataStorageType
+    # TODO: finish DataStorageType
     if   self.data.dtype == 'byte':        # DataStorageType
       f.write(np.uint32(1)) 
     elif self.data.dtype == 'ubyte':
@@ -180,18 +201,18 @@ class viffFile():
     f.write(np.uint32(0))                  # FSpare1
     f.write(np.uint32(0))                  # FSpare2
     f.write(b'\x00'*404)                   # Reserve
-    f.write(self.data)
+    f.write(self.data) # TODO: update to make this more rigorous 
     f.close()
     return
   
 
-''' demonstration of deployment '''
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description='viffFile reader example')
+  """ Demonstration """
+  parser = argparse.ArgumentParser(description='viff reader example')
   parser.add_argument('--file', type=str, help='filename (default=None)')
   args = parser.parse_args()
 
-  test = viffFile(args.file)
+  test = viff(args.file)
 
   plt.imshow(test.data[0,0,:,:])
   plt.show()
